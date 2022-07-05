f900aac0a1cc9e5fef588985c91b272d7ea805ad
Ariel Veras <ariolvs123@gmail.com>
2022-07-05 17:34:45 -0400

    [tarefa] criando conexão e classe startup

---
+++ AppBrasilCash/Startup.cs
@@ -0,0 +1,39 @@
+﻿using AppCashBrasil.Infra;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppBrasilCash
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(op =>
+            {
+                op.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
