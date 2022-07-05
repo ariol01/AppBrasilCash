da1e26af46e05c3e6cf1dce5f6ea4e8d6b602405
Ariel Veras <ariolvs123@gmail.com>
2022-07-05 17:51:57 -0400

    [tarefa] adicinando pacotes do fluent validation e criando validações.

---
+++ AppBrasilCash/Models/Validation/AccountValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace AppBrasilCash.Models.Validation
+{
+    public class AccountValidation : AbstractValidator<Account>
+    {
+        public AccountValidation()
+        {
+            RuleFor(x => x.TaxId).NotNull();
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(60);
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(8);
+        }
+    }
+}
