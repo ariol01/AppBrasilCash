f6639576dd9d81f002bb75516594b272fe4744d2
Ariel Veras <ariolvs123@gmail.com>
2022-07-06 13:10:45 -0400

    [tarefa] Configurando application

---
+++ AppBrasilCash.Application/Service/AccountAppService.cs
@@ -0,0 +1,34 @@
+﻿using AppBrasilCash.Application.Interface.AppService;
+using AppBrasilCash.Models;
+using AppBrasilCash.Models.Interface.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBrasilCash.Application.Service
+{
+    public class AccountAppService : IAccountAppService
+    {
+        private readonly IAccountService _accountService;
+        public AccountAppService(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        public async Task<Account> Get(int id)
+        {
+          return await _accountService.Get(id);
+        }
+
+        public Task<IList<Account>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Post(Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
